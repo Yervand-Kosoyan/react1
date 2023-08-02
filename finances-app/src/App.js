8137d5649b8b2bbba7503d45c323b77b7fc4ca33
Yervand-Kosoyan <yervand.kosoyan@gmail.com>
2023-08-02 03:06:21 +0200

    Created cost component.

--- finances-app/src/App.js
+++ finances-app/src/App.js
@@ -1,7 +1,42 @@
+import CostItem from "./componens/CostItem";
 function App() {
+  const costs = [
+    {
+      date: new Date(2023, 7, 1),
+      description: "Something3",
+      amount: 1591,
+    },
+    {
+      date: new Date(2023, 6, 27),
+      description: "Something2",
+      amount: 123,
+    },
+    {
+      date: new Date(2023, 6, 25),
+      description: "Something1",
+      amount: 111,
+    },
+  ];
+
   return (
     <div className="App">
       <h1>Hello!</h1>
+
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      />
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      />
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      />
     </div>
   );
 }
